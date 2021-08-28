5c935106119116ae0e04bb600220bbdd0c3b7596
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-08-28 08:53:35 +0300

    pushing navigation rauting paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { UserProfilesComponent } from './user-profiles/user-profiles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path : 'home', component : LandingPageComponent},
+  {path : 'user-profile', component : UserProfilesComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
