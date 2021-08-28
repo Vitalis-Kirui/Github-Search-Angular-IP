982c89a8fd66591008d6f444d16bc8be50f98aa3
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-08-29 01:49:59 +0300

    pushing html code to display profile details

--- src/app/user-class.ts
+++ src/app/user-class.ts
@@ -4,7 +4,6 @@ export class UserClass {
         public login : string,
         public followers: number,
         public avatar_url: string, 
-        public name: string, 
         public bio : string, 
         public location : string,
         public following : number,){
