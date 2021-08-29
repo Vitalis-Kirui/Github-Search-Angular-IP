f23d7a759a6fdd64f6f4af8ca5f8f6d35b3d553f
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-08-29 17:12:01 +0300

    pushing updates for repository html content

--- src/app/user-class.ts
+++ src/app/user-class.ts
@@ -6,7 +6,10 @@ export class UserClass {
         public avatar_url: string, 
         public bio : string, 
         public location : string,
-        public following : number,){
+        public following : number,
+        public public_repos : number,
+        public forks_count : number
+        ){
 
     };
 
